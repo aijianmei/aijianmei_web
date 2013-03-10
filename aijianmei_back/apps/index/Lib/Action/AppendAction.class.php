2b13dbc0613dd098ef03a35d33950b8f72e638da
kevin <422594175@qq.com>
2013-03-10 16:48:10 +0800

    article list

--- aijianmei_back/apps/index/Lib/Action/AppendAction.class.php
+++ aijianmei_back/apps/index/Lib/Action/AppendAction.class.php
@@ -19,7 +19,7 @@ class AppendAction extends Action {
 	
 	public function articleList()
 	{
-		$order = isset($_GET['order']) ? t($_GET['order']) : '';
+		$order = isset($_GET['order']) ? t($_GET['order']) : 'create_time';
 		$id = intval($_GET['id']);
 		$cate = M('article_category')->where(array('channel'=>'4'))->findAll();
 		foreach($cate as $c) {
@@ -28,7 +28,17 @@ class AppendAction extends Action {
 			$cate_id[] = $c['id'];
 		}
 		$map['category_id'] = $id ? $id : array('in', implode(',', $cate_id));
-		$articles = M('article')->where(array('category_id'=>$id))->order("$order desc")->findAll();
+		$articleCount = M('article')->where(array('category_id'=>$id))->count();
+		$pager = api('Pager');
+		$pager->setCounts($articleCount);
+		$pager->setList(10);
+		$pager->makePage();
+		$pageArray = (array)$pager;
+		$this->assign('pager', $pageArray);
+		$from = ($pager->pg-1) * $pager->countlist;
+		
+		$articles = M('article')->where(array('category_id'=>$id))->order("$order desc")->limit("$from,$pager->countlist")->findAll();
+		
 		$this->assign('articles', $articles);
 		$this->assign('categories', $realCate);
 		$this->assign('cssFile', 'add');
