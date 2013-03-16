f65d294f9b419972a110962bf74474c73ac6adbe
yishudeyu <yishudeyu@163.com>
2013-03-16 11:29:23 +0800

    锻炼计划页面滑动图片修正

--- aijianmei_back/apps/index/Lib/Action/PlanAction.class.php
+++ aijianmei_back/apps/index/Lib/Action/PlanAction.class.php
@@ -24,6 +24,17 @@ class PlanAction extends Action {
 	
 	public function plan_loss()
 	{
+		//banner 滚动图片列表
+		 $change_1="01.jpg";
+		 $change_2="02.jpg";
+		 $change_3="07.jpg";
+		 $change_4="16.jpg";
+		 $this->assign('change_1',$change_1);
+		 $this->assign('change_2',$change_2);
+		 $this->assign('change_3',$change_3);
+		 $this->assign('change_4',$change_4);
+		//-------END--------
+		
 		$this->assign('cssFile', 'plan');
 		if($_GET['sex']=='m') {
 			$this->display('plan_loss_m');
@@ -36,6 +47,17 @@ class PlanAction extends Action {
 	
 	public function plan_build()
 	{
+		//banner 滚动图片列表
+		 $change_1="01.jpg";
+		 $change_2="02.jpg";
+		 $change_3="07.jpg";
+		 $change_4="16.jpg";
+		 $this->assign('change_1',$change_1);
+		 $this->assign('change_2',$change_2);
+		 $this->assign('change_3',$change_3);
+		 $this->assign('change_4',$change_4);
+		//-------END--------
+		
 		$this->assign('cssFile', 'plan');
 		if($_GET['sex']=='m') {
 			$this->display('plan_build_m');
@@ -48,6 +70,17 @@ class PlanAction extends Action {
 
 	public function coach()
 	{
+		 //banner 滚动图片列表
+		 $change_1="01.jpg";
+		 $change_2="02.jpg";
+		 $change_3="07.jpg";
+		 $change_4="16.jpg";
+		 $this->assign('change_1',$change_1);
+		 $this->assign('change_2',$change_2);
+		 $this->assign('change_3',$change_3);
+		 $this->assign('change_4',$change_4);
+		//-------END--------
+		
 		$coach = D('Article')->getCoach();
 		$this->assign('coach', $coach);
 		$this->assign('cssFile', 'plan');
@@ -56,6 +89,17 @@ class PlanAction extends Action {
 
 	public function gym()
 	{
+		//banner 滚动图片列表
+		 $change_1="01.jpg";
+		 $change_2="02.jpg";
+		 $change_3="07.jpg";
+		 $change_4="16.jpg";
+		 $this->assign('change_1',$change_1);
+		 $this->assign('change_2',$change_2);
+		 $this->assign('change_3',$change_3);
+		 $this->assign('change_4',$change_4);
+		//-------END--------
+		
 		$gym = D('Article')->getGym();
 		$this->assign('gym', $gym);
 		$this->assign('cssFile', 'plan');
