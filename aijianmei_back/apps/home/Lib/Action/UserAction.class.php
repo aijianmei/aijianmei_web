868cc76bd8e260e7257c1dc62488ef52df795f01
kevin <jonimi@sina.com>
2013-03-23 02:09:47 +0800

    头部更新

--- aijianmei_back/apps/home/Lib/Action/UserAction.class.php
+++ aijianmei_back/apps/home/Lib/Action/UserAction.class.php
@@ -68,7 +68,7 @@ class UserAction extends Action {
         $groups = M('friend_group')->where(array('uid'=>$this->mid))->findAll();
         $this->assign('groups', $groups);
         
-        $members = M('user')->findAll();
+        $members = M('user')->limit('0,6')->findAll();
         $this->assign('members', $members);
         
         $videos = M('video')->limit('0,3')->order('create_time desc')->findAll();
