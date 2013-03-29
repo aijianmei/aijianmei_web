a65158cc8acac0e93950e8ce4db45ddbc300a447
kontem <ckontem@gmail.com>
2013-03-29 16:44:15 +0800

    缓存

---
+++ aijianmei_back/_runtime/~temp/f/3/~@f31f205409e7e9f839efb2f139f2c585.php
@@ -0,0 +1,3 @@
+<?php
+//000000000000a:12:{s:8:"weibo_id";s:2:"12";s:3:"uid";s:2:"19";s:7:"content";s:12:"哈哈哈哈";s:5:"ctime";s:10:"1363190481";s:4:"from";s:1:"0";s:7:"comment";s:1:"0";s:12:"transpond_id";s:1:"0";s:9:"transpond";s:1:"0";s:4:"type";s:1:"0";s:9:"type_data";N;s:9:"from_data";N;s:5:"isdel";s:1:"0";}
+?>
