58cb8b7b47ab729241864a6db6971c0473a2ce40
kontem <ckontem@gmail.com>
2013-03-29 16:22:46 +0800

    健身计划模块 新增分页功能

--- aijianmei_back/apps/index/Lib/Model/ArticleModel.class.php
+++ aijianmei_back/apps/index/Lib/Model/ArticleModel.class.php
@@ -15,7 +15,7 @@ class ArticleModel extends Model {
     {
         $sql = "select d.id,d.title,d.img,d.content,d.create_time,d.like,d.unlike,v.id as vid,v.title as vtitle,v.link,v.intro from ai_daily as d
                 left join ai_daily_video  as v on v.daily_id=d.id 
-                where d.channel=".$channel." ORDER BY d.create_time DESC";
+                where d.channel=".$channel." ORDER BY d.create_time DESC  limit ".$limit.",".$nums." ";
         
         $result = M('')->query($sql);
         
