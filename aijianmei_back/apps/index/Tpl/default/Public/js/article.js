1d920a2ee43711775978e4648086e9159fc7b722
WeiMian <996465114@qq.com>
2013-04-02 12:16:40 +0800

    文章页面添加了抢沙发文字

--- aijianmei_back/apps/index/Tpl/default/Public/js/article.js
+++ aijianmei_back/apps/index/Tpl/default/Public/js/article.js
@@ -1,67 +1,4 @@
 $(function(){
-	// //页面选择
-	// $(".lay_page a").click(function(){
-	// 	$(".lay_page a").removeClass("choice_page")
-	// 	$(this).addClass("choice_page")
-	// 	if($(".lay_page a").index(this) == 0){
-	// 		$(".pre_page").css({'color':'#ccc'})
-	// 	}
-	// 	else{
-	// 		$(".pre_page").css({'color':'#656565','cursor':'pointer'})
-	// 	}
-	// 	if($(".lay_page a").index(this) == 4){
-	// 		$(".next_page").css({'color':'#ccc','cursor':'default'})
-	// 	}
-	// 	else{
-	// 		$(".next_page").css({'color':'#656565'})
-	// 	}
-	// })
-	
-
-	//下一页、上一页
-	//4==$(".tab_2 .lay_page a").length-1
-	// $(".next_page").click(function(){
-	// 	var index = $(".choice_page").index();
-	// 	if(index != 4){
-	// 		index++;
-	// 		$(".pre_page").css({'color':'#656565','cursor':'pointer'})
-	// 		$(".lay_page a").removeClass("choice_page")
-	// 		$(".lay_page a").eq(index).addClass("choice_page")
-	// 	}
-	// 	if(index == 4){
-	// 		$(".next_page").css({'color':'#ccc','cursor':'default'})
-	// 	}
-	// })
-	// $(".pre_page").click(function(){
-	// 	var index = $(".choice_page").index();
-	// 	if(index != 0){
-	// 		index--;
-	// 		$(".next_page").css({'color':'#656565','cursor':'pointer'})
-	// 		$(".lay_page a").removeClass("choice_page")
-	// 		$(".lay_page a").eq(index).addClass("choice_page")
-	// 	}
-	// 	if(index == 0){
-	// 		$(".pre_page").css({'color':'#ccc','cursor':'default'})
-	// 	}
-	// })
-
-	//全部评论
-	$(".comment_1").click(
-		function(){
-			$(".comment_2").removeClass("comment_3")
-			$(this).addClass("comment_3")
-			$(".tab_1").css("display","block")
-			$(".tab_2").css("display","none")
-		}
-	)
-	$(".comment_2").click(
-		function(){
-			$(".comment_1").removeClass("comment_3")
-			$(this).addClass("comment_3")
-			$(".tab_2").css("display","block")
-			$(".tab_1").css("display","none")
-		}
-	)
 
 	//praise
 	$(".sprite_8").add(".sprite_9").click(function(){
@@ -69,16 +6,6 @@ $(function(){
 		time++;
 		$(this).children().html(time)
 	})
-
-	//页面下拉列表
-	$(".read_time").hover(
-		function(){
-			$(".page_times").css("display","block")
-		},
-		function(){
-			$(".page_times").css("display","none")
-		}
-	)
 
 	//textarea focus()
 	$(".comment_inp").click(function(){
@@ -101,13 +28,17 @@ $(function(){
 		$(".sheet").css("top",0);
 	});
 
+	//消除没有人评论提示文本
+	var removetext = function(){
+		var newdom = new getdom,
+			empty_content = newdom.getElementsByClass("empty_content")[0];
+			empty_content.parentNode.style.color = 'blue'
+			// alert(empty_content.nextSibling.innerHTML)
+			if(empty_content.nextSibling.className != null){
+				empty_content.style.display = 'none';
+			}
+	}
+	removetext();
 	
 })
 
-
-// var today = function(){
-	// var time = document.getElementsByClassName("time")[0],
-		// date = new Date();
-		// time.innerHTML = [date.getFullYear(),'-',date.getMonth(),'-',date.getDate(),"&nbsp;&nbsp;",date.toLocaleTimeString()].join("");
-// }
-// setInterval('today()',1000);
