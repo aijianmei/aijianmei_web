5d03b0af1632af0c9cfee6d7581aaf105abe08c8
WeiMian <996465114@qq.com>
2013-04-03 12:13:58 +0800

    改好了图片鼠标在上面显示进入

--- aijianmei_back/apps/index/Tpl/default/Public/js/public.js
+++ aijianmei_back/apps/index/Tpl/default/Public/js/public.js
@@ -312,14 +312,14 @@ $(function(){
     
 // })	
 //鼠标移上图片显示进入
-$("li.show_enter").mouseover(function(){
-	$(this).children().children(".enter_icon").css("display","block");
-	$(this).children().children("img").css("border-color","#21ace3");
-	$(this).children().children(".v_enter").css('background','url(images/wm3.png) no-repeat 0 -490px')
+$("li .show_enter").mouseover(function(){
+	$(this).children(".enter_icon").css("display","block");
+	$(this).children("img").css("border-color","#21ace3");
+	$(this).children(".v_enter").css('background','url(images/wm3.png) no-repeat 0 -490px')
 	$(this).mouseout(function(){
-		$(this).children().children(".enter_icon").css("display","none");
-		$(this).children().children("img").css("border-color","transparent");
-		$(this).children().children(".v_enter").css('background','')
+		$(this).children(".enter_icon").css("display","none");
+		$(this).children("img").css("border-color","transparent");
+		$(this).children(".v_enter").css('background','')
 	})
 });
 //添加目录动态快---------------------------------------
