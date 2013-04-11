7a8b33a0edac1fbe1807c02f595a54dd44527fe4
WeiMian <996465114@qq.com>
2013-04-11 21:50:07 +0800

    阿萨德发烧发烧的

---
+++ aijianmei_back/shop/temp/static_caches/recommend_goods.php
@@ -0,0 +1,31 @@
+<?php
+$data = array (
+  'best' => 
+  array (
+    0 => 
+    array (
+      'goods_id' => '1',
+      'sort_order' => '100',
+    ),
+  ),
+  'new' => 
+  array (
+    0 => 
+    array (
+      'goods_id' => '1',
+      'sort_order' => '100',
+    ),
+  ),
+  'hot' => 
+  array (
+    0 => 
+    array (
+      'goods_id' => '1',
+      'sort_order' => '100',
+    ),
+  ),
+  'brand' => 
+  array (
+  ),
+);
+?>
