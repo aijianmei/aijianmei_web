812f9c09bc22ef5b32d6cc48aa1caeb14a264805
WeiMian <996465114@qq.com>
2013-04-12 13:40:40 +0800

    莫名文件

--- aijianmei_back/shop/temp/query_caches/sqlcache_config_file_445f2c63abbbcf2e230719f8a9f2a19e.php
+++ aijianmei_back/shop/temp/query_caches/sqlcache_config_file_445f2c63abbbcf2e230719f8a9f2a19e.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1365744785;
+$this->mysql_config_cache_file_time = 1365745119;
 $this->timeline = 0;
 $this->timezone = 28800;
 $this->platform = 'OTHER';
