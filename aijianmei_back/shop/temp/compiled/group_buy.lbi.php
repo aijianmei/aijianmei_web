653feb85af6b681ad2daa802e9c31918364c7996
WeiMian <996465114@qq.com>
2013-04-12 21:08:16 +0800

    阿斯蒂芬gas的

--- aijianmei_back/shop/temp/compiled/group_buy.lbi.php
+++ aijianmei_back/shop/temp/compiled/group_buy.lbi.php
@@ -4,13 +4,13 @@
   <h3><span><?php echo $this->_var['lang']['group_buy_goods']; ?></span><a href="group_buy.php"><img src="themes/default/images/more.gif"></a></h3>
     <div class="centerPadd">
     <div class="clearfix goodsBox" style="border:none;">
-      <?php $_from = $this->_var['group_buy_goods']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'goods_0_33983000_1365746045');if (count($_from)):
-    foreach ($_from AS $this->_var['goods_0_33983000_1365746045']):
+      <?php $_from = $this->_var['group_buy_goods']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'goods_0_92129800_1365773768');if (count($_from)):
+    foreach ($_from AS $this->_var['goods_0_92129800_1365773768']):
 ?>
       <div class="goodsItem">
-           <a href="<?php echo $this->_var['goods_0_33983000_1365746045']['url']; ?>"><img src="<?php echo $this->_var['goods_0_33983000_1365746045']['thumb']; ?>" alt="<?php echo htmlspecialchars($this->_var['goods_0_33983000_1365746045']['goods_name']); ?>" class="goodsimg" /></a><br />
-					 <p><a href="<?php echo $this->_var['goods_0_33983000_1365746045']['url']; ?>" title="<?php echo htmlspecialchars($this->_var['goods_0_33983000_1365746045']['goods_name']); ?>"><?php echo htmlspecialchars($this->_var['goods_0_33983000_1365746045']['short_style_name']); ?></a></p>
-           <font class="shop_s"><?php echo $this->_var['goods_0_33983000_1365746045']['last_price']; ?></font>
+           <a href="<?php echo $this->_var['goods_0_92129800_1365773768']['url']; ?>"><img src="<?php echo $this->_var['goods_0_92129800_1365773768']['thumb']; ?>" alt="<?php echo htmlspecialchars($this->_var['goods_0_92129800_1365773768']['goods_name']); ?>" class="goodsimg" /></a><br />
+					 <p><a href="<?php echo $this->_var['goods_0_92129800_1365773768']['url']; ?>" title="<?php echo htmlspecialchars($this->_var['goods_0_92129800_1365773768']['goods_name']); ?>"><?php echo htmlspecialchars($this->_var['goods_0_92129800_1365773768']['short_style_name']); ?></a></p>
+           <font class="shop_s"><?php echo $this->_var['goods_0_92129800_1365773768']['last_price']; ?></font>
         </div>
       <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>
     </div>
--- aijianmei_back/shop/temp/query_caches/sqlcache_config_file_445f2c63abbbcf2e230719f8a9f2a19e.php
+++ aijianmei_back/shop/temp/query_caches/sqlcache_config_file_445f2c63abbbcf2e230719f8a9f2a19e.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1365771645;
+$this->mysql_config_cache_file_time = 1365774393;
 $this->timeline = 0;
 $this->timezone = 28800;
 $this->platform = 'OTHER';
