c6c45dc4d453e883e33290d35054001b13082532
WeiMian <996465114@qq.com>
2013-05-17 13:27:35 +0800

    datum.html

--- aijianmei_front_1/js/datum.js
+++ aijianmei_front_1/js/datum.js
@@ -90,6 +90,29 @@ var aijianmei = {
                 obj.children[1].style.visibility = 'hidden';
             }
         }
+    },
+    form_jugde : function(jugde_1,jugde_2,jugde_3){
+        var id_jd = [],
+            jd = [],
+            argument_len = arguments.length;
+        for(var i = 0;i < argument_len;i++){
+            id_jd[i] = aijianmei.newdom.Id(arguments[i]);
+            jd[i] = aijianmei.getobj(arguments[i]);
+        }
+        jd_len = id_jd.length;
+        for(var i = 0;i < jd_len;i++){
+            if(id_jd[i]){
+                id_jd[i].index = i;
+                id_jd[i].onfocus = function(){
+                    jd[this.index].style.display = 'none';
+                }
+                id_jd[i].onblur = function(){
+                    if(id_jd[this.index].value == ""){
+                        jd[this.index].style.display = 'block';
+                    }
+                }
+            }       
+        }
     }
 }
 var init = function(){
@@ -107,16 +130,18 @@ var init = function(){
         len_dt_weight = a_dt_weight.length,
         arr_year = [10,25,40,55],
         arr_height = [140,155,170,185],
+        arr_weight = [40,55,70,85]
         div = [],
         dt_choice_year = newdom.getElementsByClass('dt_choice_year'),
-        dt_choice_height = newdom.getElementsByClass('dt_choice_height');
+        dt_choice_height = newdom.getElementsByClass('dt_choice_height'),
+        dt_choice_weight = newdom.getElementsByClass('dt_choice_weight');
 
     // year
     for(var i = 1;i < len_dt_year - 1;i++){
         a_dt_year[i].index = i;
         for(var x = 0,y = 0;x < 15;x++){
             var  a = document.createElement('a');
-            a.className = 'dt_target';
+            a.className = 'dt_year_target';
             y = arr_year[i - 1] + x;
             a.innerHTML = y + '岁';
             dt_choice_year[i - 1].appendChild(a);//add <a> to div
@@ -133,9 +158,9 @@ var init = function(){
         a_dt_height[i].index = i;
         for(var x = 0,y = 0;x < 15;x++){
             var  a = document.createElement('a');
-            a.className = 'dt_target';
+            a.className = 'dt_height_target';
             y = arr_height[i - 1] + x;
-            a.innerHTML = y + '岁';
+            a.innerHTML = y + 'cm';
             dt_choice_height[i - 1].appendChild(a);//add <a> to div
         }
         a_dt_height[i].onmouseover = function(){
@@ -145,41 +170,148 @@ var init = function(){
             dt_choice_height[this.index - 1].style.display = 'none';
         }  
     }
-    //dt_target add click event
-    var dt_target = newdom.getElementsByClass('dt_target'),
-        len_dt_target = dt_target.length,
+    // weight
+    for(var i = 1;i < len_dt_weight - 1;i++){
+        a_dt_weight[i].index = i;
+        for(var x = 0,y = 0;x < 15;x = x + 0.5){
+            var  a = document.createElement('a');
+            a.className = 'dt_weight_target';
+            y = arr_weight[i - 1] + x;
+            a.innerHTML = y + 'kg';
+            dt_choice_weight[i - 1].appendChild(a);//add <a> to div
+        }
+        a_dt_weight[i].onmouseover = function(){
+            dt_choice_weight[this.index - 1].style.display = 'block';
+        }
+        a_dt_weight[i].onmouseout = function(){
+            dt_choice_weight[this.index - 1].style.display = 'none';
+        }  
+    } 
+    //dt_year_target add click event
+    var dt_year_target = newdom.getElementsByClass('dt_year_target'),
+        len_year_target = dt_year_target.length,
         dt_year = newdom.getElementsByClass("dt_year")[0],
         dt_year_edit = newdom.getElementsByClass("dt_year_edit")[0],
         dt_year_finish = newdom.getElementsByClass("dt_year_finish")[0],
-        dt_year_change = newdom.getElementsByClass("dt_year_change")[0];
+        dt_year_change = newdom.getElementsByClass("dt_year_change")[0],
+        dt_year_target = newdom.getElementsByClass('dt_year_target'),
+        //for height
+        dt_height_target = newdom.getElementsByClass('dt_height_target'),
+        len_height_target = dt_height_target.length,
+        dt_height = newdom.getElementsByClass("dt_height")[0],
+        dt_height_edit = newdom.getElementsByClass("dt_height_edit")[0],
+        dt_height_finish = newdom.getElementsByClass("dt_height_finish")[0],
+        dt_height_change = newdom.getElementsByClass("dt_height_change")[0],
+         //for weight
+        dt_weight_target = newdom.getElementsByClass('dt_weight_target'),
+        len_weight_target = dt_weight_target.length,
+        dt_weight = newdom.getElementsByClass("dt_weight")[0],
+        dt_weight_edit = newdom.getElementsByClass("dt_weight_edit")[0],
+        dt_weight_finish = newdom.getElementsByClass("dt_weight_finish")[0],
+        dt_weight_change = newdom.getElementsByClass("dt_weight_change")[0];
     //act_change add click event
-    var act_change = function(){
-        if(dt_year_change){
-            dt_year_change.onclick = function(){
-                dt_year.style.display = "block";
-                dt_year_edit.style.display = "none";
+    var act_change = function(obj_1,obj_2,obj){
+        if(obj){
+            obj.onclick = function(){
+                obj_1.style.display = "block";
+                obj_2.style.display = "none";
             }
         }    
     }       
     //choice_this    
-    var choice_this = function(e){
+    var choice_year_this = function(e){
         var _e = window.event ? window.event : e || arguments[0],
             _target = _e.target ? _e.target : _e.srcElement;
         dt_year_finish.innerHTML = _target.innerHTML;
         dt_year.style.display = "none";
         dt_year_edit.style.display = "block";
-        act_change();
+        act_change(dt_year,dt_year_edit,dt_year_change);
     }
-    for(var i = 0;i < len_dt_target;i++){
-        if(dt_target[i]){
-            addevent(dt_target[i],'click',choice_this)
+    var choice_height_this = function(e){
+        var _e = window.event ? window.event : e || arguments[0],
+            _target = _e.target ? _e.target : _e.srcElement;
+        dt_height_finish.innerHTML = _target.innerHTML;
+        dt_height.style.display = "none";
+        dt_height_edit.style.display = "block";
+        act_change(dt_height,dt_height_edit,dt_height_change);
+    }
+    var choice_weight_this = function(e){
+        var _e = window.event ? window.event : e || arguments[0],
+            _target = _e.target ? _e.target : _e.srcElement;
+        dt_weight_finish.innerHTML = _target.innerHTML;
+        dt_weight.style.display = "none";
+        dt_weight_edit.style.display = "block";
+        act_change(dt_weight,dt_weight_edit,dt_weight_change);
+    }
+    //year
+    for(var i = 0;i < len_year_target;i++){
+        if(dt_year_target[i]){
+            addevent(dt_year_target[i],'click',choice_year_this);
+            addevent(a_dt_year[0],'click',choice_year_this);
+            addevent(a_dt_year[0],'click',choice_year_this);
+            addevent(a_dt_year[len_dt_year - 1],'click',choice_year_this);
+        }   
+    }
+    //height
+    for(var i = 0;i < len_height_target;i++){
+        if(dt_height_target[i]){
+            addevent(dt_height_target[i],'click',choice_height_this);
+            addevent(a_dt_height[0],'click',choice_height_this);
+            addevent(a_dt_height[len_dt_height - 1],'click',choice_height_this);
+        }   
+    }
+    //weight
+    for(var i = 0;i < len_weight_target;i++){
+        if(dt_weight_target[i]){
+            addevent(dt_weight_target[i],'click',choice_weight_this);
+            addevent(a_dt_weight[0],'click',choice_weight_this);
+            addevent(a_dt_weight[len_dt_weight - 1],'click',choice_weight_this);
         }   
     }
 
 
 
+    // add tag
+    var tag_id = newdom.Id('tag'),
+        tag_class= newdom.getElementsByClass('tag')[0],
+        dt_wrap_sure = newdom.getElementsByClass('dt_wrap_sure')[0],
+        text = 'underfined';
+    //     div = document.createElement('div'),
+    //     text = '<span class="dt_sure_content">伟勉</span><span class="dt_sure_remove">×</span>>';
+    // div.className = 'dt_sure';
+    // div.innerHTML = text;
+    tag_id.onfocus = function(){
+        tag_class.style.display = "none";
+    }
+    tag_id.onblur = function(){
+        if(tag_id.value != ""){
+            //add dt_sure
+            var div = document.createElement('div');
+            text = '<span class="dt_sure_content">' + tag_id.value + '</span><span class="dt_sure_remove">×</span>';
+            div.className = 'dt_sure';
+            div.innerHTML = text;
+            dt_wrap_sure.appendChild(div);
+        }
+            
 
+        tag_id.value = "";
+        tag_class.style.display = "block";
 
+        // dt_sure_remove add click event
+        var dt_sure_remove = newdom.getElementsByClass('dt_sure_remove');
+        if(dt_sure_remove){
+            var len_dt_remove = dt_sure_remove.length;
+            for(var i = 0;i < len_dt_remove;i++){
+                dt_sure_remove[i].onclick = function(){
+                    // this.parentNode.style.display = 'none';
+                    dt_wrap_sure.removeChild(this.parentNode)
+                }
+            }
+        }
+    }
+        
+        
+    
 
     // head
     for(var i = 0;i < len_1;i++){
