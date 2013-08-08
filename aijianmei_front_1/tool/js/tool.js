ece5a50ff63bf16ff8e78e221e12cfff1308e871
Ann <1152781715@qq.com>
2013-08-08 19:38:50 +0800

    tool

---
+++ aijianmei_front_1/tool/js/tool.js
@@ -0,0 +1,196 @@
+//weight
+var changeWeight = function(input, show){
+	$(".weiSaveBtn").click(function(){
+		var innerVal = $(input).val();		
+		innerVal += "kg";
+		$(show).find(".innerText").text(innerVal);
+		
+		progress();
+	});
+	//计算进度条长度
+	var progress = function(){
+		var goal = $("#goalW").val(),
+			cur = $("#curW").val(),
+			init = $("#initW").val(),
+			allPro = Math.abs(init - goal),
+			nowPro = Math.abs(cur - goal),
+			percentage = nowPro / allPro * 100;
+			if(percentage <= 100)
+				percentage = percentage.toFixed(1) + "%";
+			else
+				percentage = "0%";
+			
+			$(".innerPro").css("width", percentage);
+			$(".percent").text(percentage);
+	}
+	
+}
+changeWeight("#initW", ".initWeight");
+changeWeight("#goalW", ".goalWeight");
+changeWeight("#curW", ".curWeight");
+
+//datepicker
+var dateVar = (new Date()).getFullYear() + '-' + ((new Date()).getMonth() + 1) + '-' + (new Date()).getDate();	
+$(".curDate").text(dateVar);//set date of current weight.
+			
+var setDateVal = function (datepicker){
+		$( datepicker ).datepicker();
+		$( datepicker).datepicker( "option", "dateFormat","yy-mm-dd" );
+		$( datepicker).datepicker( "setDate",dateVar );
+	},
+	//点击左右加减一天
+	toggleDateVal = function (toggleDate){
+		$(toggleDate).click(function(){		
+			var utc;
+			dateVar = $(this).parent().find("input").val();
+			
+			if($(this).hasClass("preDate")){	
+				utc = Date.parse(dateVar)  - 24*60*60*1000;
+			}
+			else if($(this).hasClass("nextDate")){
+				utc = Date.parse(dateVar)  + 24*60*60*1000;
+			}
+			
+			var date = new Date(utc),				
+				y = date.getFullYear(),
+				m = date.getMonth() + 1,
+				d = date.getDate();
+			dateVar = y + '-' + m + '-' + d;
+			$(this).parent().find("input").datepicker( "setDate",dateVar );
+		});
+	};
+
+setDateVal(".calendar");
+toggleDateVal(".toggleDate");
+
+//高亮样式
+var highLight = function(ele, curClass){
+	$(ele).children().bind("click", function(){		
+		$(this).addClass(curClass).siblings().removeClass(curClass);
+	});
+}
+
+highLight(".chartDate", "curChart");
+highLight(".rankLine", "curRanking");
+highLight(".siderBtn", "curGroup");
+
+
+//tab
+$(".dataTab").children().bind("click", function(){
+	var _index = $(this).index();
+	$(this).find("span").css("display","none");
+	$(this).siblings().find("span").css("display", "block");
+	$(".userArea").eq(_index).css("display","block").siblings().css("display","none");
+})
+/* 
+//upload
+$(".uploadArea").bind({
+	mouseenter : function(){
+		$(this).find(".uploadCover").slideDown();
+		$(this).find(".clickUpload").slideDown();
+	},
+	mouseleave : function(){
+		$(this).find(".uploadCover").slideUp();
+		$(this).find(".clickUpload").hide();
+	}
+})
+ */
+//用户 
+var userlist=[{
+				"part" : "我的锻炼",
+				"lists" : 
+					["卧推", "卧推卧推", "卧推卧推上肢", "上肢1","上肢a", "上肢b", 
+					 "上肢v", "上肢d", "上肢e", "上肢上肢上肢", "上肢上肢上肢上肢上肢上肢", "上肢f",
+					 "上肢r", "上肢", "上肢", "上肢","上肢", "上肢名字很长很长很长很长很长很","卧推",
+					 "卧推卧推", "卧推卧推上肢", "上肢1","上肢a", "上肢b", 
+					 "上肢v", "上肢d", "上肢e", "上肢上肢上肢", "上肢上肢上肢上肢上肢上肢", "上肢f",
+					 "上肢r", "上肢", "上肢", "上肢","上肢", "上肢名字很长很长很长很长很长很"
+					]
+			}];
+//更多动作选择
+$.getJSON("json/actions.json", function(data){
+	$.merge(userlist,data);
+	data = userlist;
+	var tmpMenu = [],
+		listsArr = [],
+		_objLen = data.length,
+		_listLen = data[0].lists.length;
+
+	for(var i=0;i<_objLen;i++){
+		var obj = data[i];	
+		tmpMenu.push('<li class="moreList">' + obj.part + '</li>');		
+	}
+	
+	for(var j=0;j<_listLen;j++){
+		listsArr.push('<li class="showList">' + data[0].lists[j] + '</li>');
+	}
+
+	$(".moreSider").append(tmpMenu.join(""));
+	$(".moreRight>ul").append(listsArr.join(""));
+	
+}).done(function(data){
+		$.merge(userlist,data);
+		data = userlist;
+		$(".moreList").first().addClass("curList");
+		highLight(".moreSider", "curList");
+		
+		$(".moreList").bind("click", function(){
+			$(".moreRight>ul").empty();
+			var _index = $(this).index(),
+			listsArr = [],
+			_listLen = data[_index].lists.length;
+			for(var j=0;j<_listLen;j++){
+				listsArr.push('<li class="showList">' + data[_index].lists[j] + '</li>');
+			}
+			$(".moreRight>ul").append(listsArr.join(""));
+		});
+		
+		$(".moreBtn").click(function(){
+			$(".moreAct").slideDown();
+		});
+		
+		$(".hideBtn, .showList").live("click",function(){
+			if($(this).hasClass("showList")){
+				var txt = $(this).text(),
+					navTxt = [],
+					_navLen = $(".actNav").find("li").length,
+					i = 0;
+
+				for(i;i<_navLen;i++){
+					navTxt[i] = $(".actNav").find("li").eq(i).text();
+					if(txt == navTxt[i]){
+						$(".actNav").find(".curAct").removeClass("curAct");
+						$(".actNav").find("li").eq(i).addClass("curAct");
+						break;
+					}
+				}
+				if(i==_navLen){
+					$(".actNav").find(".curAct").removeClass("curAct");					
+					$(".actNav").prepend('<li class="curAct">' + txt + '<span></span></li>');
+					listLength();
+				}	
+			}
+			$(".moreAct").slideUp();
+		});
+		$(".actNav>li").live("click", function(){
+			$(this).addClass("curAct").siblings().removeClass("curAct");
+		});
+});
+
+//控制导航栏显示
+var listLength = function(){
+	var actList = [],
+		allList = $(".actNav").find("li").last().index(),
+		anvLength = 0;		
+	for(var i=0;i<=allList;i++){
+		actList[i] = $(".actNav>li").eq(i).text();
+		anvLength += actList[i].length * 18 +32;
+		while(anvLength > 800){
+			anvLength -= ($(".actNav").find("li").last().width() + 32);
+			$(".actNav>li").last().remove();
+			if(anvLength <= 800)
+				break;
+		}
+	}
+}
+listLength();
