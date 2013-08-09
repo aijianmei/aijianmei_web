17cca34eabfd36b5770a6b1d26d10696b97d8385
Ann <1152781715@qq.com>
2013-08-09 17:08:20 +0800

    Tool

--- aijianmei_front_1/tool/js/tool.js
+++ aijianmei_front_1/tool/js/tool.js
@@ -1,4 +1,5 @@
 //weight
+var actionVal='';
 var changeWeight = function(input, show){
 	$(".weiSaveBtn").click(function(){
 		var innerVal = $(input).val();		
@@ -9,12 +10,34 @@ var changeWeight = function(input, show){
 	});
 	//计算进度条长度
 	var progress = function(){
-		var goal = $("#goalW").val(),
-			cur = $("#curW").val(),
-			init = $("#initW").val(),
+		var goal = $("#goalW").val(),//目标的数值
+			cur = $("#curW").val(),//当前数值
+			init = $("#initW").val(),//原始数值
 			allPro = Math.abs(init - goal),
-			nowPro = Math.abs(cur - goal),
+			nowPro =Math.abs(cur - goal),
 			percentage = nowPro / allPro * 100;
+			
+			/* add by kontem 20130808 start*/
+			if(goal*1 > init*1){//目标数值大于原始值 增重
+				if(cur*1 < init*1){//当前数值 < 原始数值 无用功直接 0%
+					percentage="0%";
+				}else{//否则计算对应完成度的百分比
+					percentage = (cur-init) / (goal-init) * 100;
+				}
+				if(cur*1>goal*1){//当前数值>目标的数值 则目标已经超标完成 直接是100%
+					percentage=100;
+				}
+			}else{//目标数值大于原始值 减肥
+				if(cur*1 < init*1 && cur*1>goal*1){//当前数值 < 原始数值  并且大于目标值 则 减肥有效果 仍需继续努力
+					percentage = (init-cur) / (init-goal) * 100;
+				}else if(cur*1 < init*1 && cur*1<goal*1 ){ //当前数值 小于原始值 并且小于目标值 减肥完美超额完成...估计要增肥了
+					percentage = 100;//完成度自然100..
+				}else if(cur*1>init*1){//当前数值 > 原始数值  好吧你减肥失败了 增重成功了...完成度0 哭也没用啊 混蛋
+					percentage="0%";
+				}
+			}
+			/* add by kontem 20130808 end*/
+			
 			if(percentage <= 100)
 				percentage = percentage.toFixed(1) + "%";
 			else
@@ -36,6 +59,7 @@ $(".curDate").text(dateVar);//set date of current weight.
 var setDateVal = function (datepicker){
 		$( datepicker ).datepicker();
 		$( datepicker).datepicker( "option", "dateFormat","yy-mm-dd" );
+		$(datepicker).datepicker('option', 'maxDate', '0');
 		$( datepicker).datepicker( "setDate",dateVar );
 	},
 	//点击左右加减一天
@@ -150,6 +174,7 @@ $.getJSON("json/actions.json", function(data){
 		});
 		
 		$(".hideBtn, .showList").live("click",function(){
+			//若所选动作已存在于导航栏，则直接给高亮显示
 			if($(this).hasClass("showList")){
 				var txt = $(this).text(),
 					navTxt = [],
@@ -160,6 +185,7 @@ $.getJSON("json/actions.json", function(data){
 					navTxt[i] = $(".actNav").find("li").eq(i).text();
 					if(txt == navTxt[i]){
 						$(".actNav").find(".curAct").removeClass("curAct");
+						actionVal=$(".actNav").find("li").eq(i).text();
 						$(".actNav").find("li").eq(i).addClass("curAct");
 						break;
 					}
@@ -170,9 +196,28 @@ $.getJSON("json/actions.json", function(data){
 					listLength();
 				}	
 			}
+
 			$(".moreAct").slideUp();
+
 		});
+		
+		//选择动作“更多”左右换页
+		$(".selectMore").bind("click", function(){
+			var nowTop = $("#actionsList").css("top"),
+				ulHeight = $("#actionsList").height();
+			nowTop = parseInt(nowTop);
+			
+			if($(this).hasClass("preSelect") && nowTop<0){
+				nowTop += 320;
+			}
+			else if($(this).hasClass("nextSelect") && (nowTop>(320-ulHeight)))
+				nowTop -= 320;
+			$("#actionsList").css("top", nowTop+"px");
+		})
+		
+		//导航栏当前高亮显示
 		$(".actNav>li").live("click", function(){
+			actionVal=$(this).text();
 			$(this).addClass("curAct").siblings().removeClass("curAct");
 		});
 });
@@ -194,3 +239,5 @@ var listLength = function(){
 	}
 }
 listLength();
+
+
